7d77ff2b9fb4595203f8b0398b8149a5fe4e4d22
amasenov <amasenov@outlook.com>
2017-12-13 20:33:45 +0100

    v. 1.1
    first and simple API

--- Node.js/firstAPI/server.js
+++ Node.js/firstAPI/server.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var ingredients = [
+    {
+        "id":"303aMa",
+        "text":"Pasta"
+    },
+    {
+        "id":"xkY973",
+        "text":"Cheese"
+    },
+    {
+        "id":"jpCu48",
+        "text":"Bacon"
+    },
+    {
+        "id":"pt34JL1",
+        "text":"Mushroom"
+    }
+];
 
 app.get('/', function(request, response) {
-    response.send('My first API!');
+    response.send(ingredients);
 });
 
-app.get('/nice', function(req, res) {
-    res.send('This API is nice');
+app.post('/', function(request, response) {
+    var ingredient = request.body;
+    if (!ingredient || ingredient.text === "") {
+        response.status(500).send({error: "Your ingredient must have text"});
+    } else {
+        ingredients.push(ingredient);
+        response.status(200).send(ingredients);
+    }
 });
 
 app.listen(3000, function() {
