5553d34363567d01366e27c7193344fd7d25d341
amasenov <amasenov@outlook.com>
2017-12-14 22:00:50 +0100

    v. 1.2
    first and simple API

--- Node.js/firstAPI/server.js
+++ Node.js/firstAPI/server.js
@@ -24,17 +24,60 @@ var ingredients = [
     }
 ];
 
-app.get('/', function(request, response) {
+app.get('/ingredients', function(request, response) {
     response.send(ingredients);
 });
 
-app.post('/', function(request, response) {
+app.post('/ingredients', function(request, response) {
     var ingredient = request.body;
     if (!ingredient || ingredient.text === "") {
         response.status(500).send({error: "Your ingredient must have text"});
     } else {
         ingredients.push(ingredient);
         response.status(200).send(ingredients);
+    }
+});
+
+app.delete('/ingredients/:ingredientId', function(request, response){
+    var objFound = false;
+    
+    var newIngredients = ingredients.filter(function(ing) {
+        if (ing.id === request.params.ingredientId) {
+            objFound = true;
+        }
+        return ing.id !== request.params.ingredientId;
+    });
+    ingredients = newIngredients;
+
+    if (!objFound) {
+        response.status(500).send({error:"Ingredient id not found"});
+    } else {
+        response.send(ingredients);
+    }
+});
+
+app.put('/ingredients/:ingredientId', function(request, response) {
+
+    var newText = request.body.text;
+    var newIngredients = [];
+    if (!newText || newText === "") {
+        response.status(500).send({error: "You must provide ingredient text"});
+    } else {
+        var objFound = false;
+        newIngredients = ingredients.map(function(ing) {
+            if(ing.id === request.params.ingredientId) {
+                ing.text = newText;
+                objFound = true;
+            }
+            return ing;
+        });
+        ingredients = newIngredients;
+
+        if (!objFound) {
+            response.status(500).send({error:"Ingredient id not found"});
+        } else {
+            response.send(ingredients);
+        }
     }
 });
 
